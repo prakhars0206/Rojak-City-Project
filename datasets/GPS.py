cb6f69e175660a7e1f86239233b3123bad5de9ab
Aisyah <aisyah.uk@gmail.com>
2025-11-01 13:40:33 +0000

    GPS data added

---
+++ datasets/GPS.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import csv
+
+baseurl = 'https://tfe-opendata.com/api/v1/timetables/36235979'
+
+r = requests.get(baseurl)
+
+load = r.json()
+print(load)
+
+with open("GPS.csv", "w", newline='') as file:
+    f = csv.writer(file)
+
+    # Write header row
+    f.writerow(['stop_id', 'stop_name', 'service_name', 'destination',
+                'note_id', 'valid_from', 'day', 'time'])
+
+    # Write data rows
+    for departure in load['departures']:
+        f.writerow([
+            load['stop_id'],
+            load['stop_name'],
+            departure['service_name'],
+            departure['destination'],
+            departure['note_id'],
+            departure['valid_from'],
+            departure['day'],
+            departure['time']
+        ])
+print(f"Written {len(load['departures'])} departures to GPS.csv")
+
+
